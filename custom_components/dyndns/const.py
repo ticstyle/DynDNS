73349ba59a205c36dd858cea1a70a33942ec2644
dualityps <4126225+ticstyle@users.noreply.github.com>
2026-07-29 15:22:03 +0200

    Refactor constants to use Final type hints
    Added type hints for constants in const.py.

--- custom_components/dyndns/const.py
+++ custom_components/dyndns/const.py
@@ -1,3 +1,14 @@
+# const.py
 """Constants for the DynDNS integration."""
 
-DOMAIN = "dyndns"
+from typing import Final
+
+DOMAIN: Final = "dyndns"
+
+CONF_SERVER: Final = "server"
+CONF_HOSTNAME: Final = "hostname"
+CONF_USERNAME: Final = "username"
+CONF_PASSWORD: Final = "password"
+
+DEFAULT_SERVER: Final = "test.domain.com"
+UPDATE_INTERVAL_MINUTES: Final = 15
