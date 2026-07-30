924b5137b8f8f7404767dec8458f1ce3694e1d91
dualityps <4126225+ticstyle@users.noreply.github.com>
2026-07-30 10:36:19 +0200

    Add protocol constants for dyndns configuration

--- custom_components/dyndns/const.py
+++ custom_components/dyndns/const.py
@@ -5,11 +5,15 @@ from typing import Final
 
 DOMAIN: Final = "dyndns"
 
+CONF_PROTOCOL: Final = "protocol"
 CONF_SERVER: Final = "server"
 CONF_HOSTNAME: Final = "hostname"
 CONF_USERNAME: Final = "username"
 CONF_PASSWORD: Final = "password"
 CONF_UPDATE_INTERVAL: Final = "update_interval"
+
+PROTOCOL_DYNDNS2: Final = "dyndns2"
+SUPPORTED_PROTOCOLS: Final = [PROTOCOL_DYNDNS2]
 
 DEFAULT_SERVER: Final = "cloud.test.org"
 DEFAULT_UPDATE_INTERVAL_MINUTES: Final = 15
